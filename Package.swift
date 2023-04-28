281096fcb837ad8b21bfa6a9305f24c123285626
Packiaseelan S <packiaseelan14@gmail.com>
2023-04-28 12:22:15 +0530

    init commit

---
+++ Package.swift
@@ -0,0 +1,21 @@
+// swift-tools-version: 5.8
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let name = "Pivot"
+let version = "V1.0.0"
+let url = "https://github.com/Packiaseelan/Pivot/releases/download/\(version)/\(name).xcframework.zip"
+let checksum = "6c3737371c977efa9bfd19fb7f976d559932e776e9da7cec4cba42bfe88e7720"
+
+let package = Package(
+    name: name,
+    products: [
+        .library(name: name, targets: [name]),
+    ],
+    targets: [
+            .binaryTarget(name: name, url: url, checksum: checksum),
+//            .binaryTarget(name: name, path: "../output/\(name).xcframework")
+        ],
+        swiftLanguageVersions: [.v5]
+)
