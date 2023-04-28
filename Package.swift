d7353cd8205f800900b86af5907b562baf564d15
Packiaseelan S <packiaseelan14@gmail.com>
2023-04-28 12:28:05 +0530

    no message

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let name = "Pivot"
 let version = "V1.0.0"
 let url = "https://github.com/Packiaseelan/Pivot/releases/download/\(version)/\(name).xcframework.zip"
-let checksum = "6c3737371c977efa9bfd19fb7f976d559932e776e9da7cec4cba42bfe88e7720"
+let checksum = "01eef5345a3e8229346f214e969f024bfa6794b5585d93089e956d00ae72419e"
 
 let package = Package(
     name: name,
