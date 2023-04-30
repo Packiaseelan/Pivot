3fbffe8e0734b73fe12b313c32968adc8ede452c
Packiaseelan S <packiaseelan14@gmail.com>
2023-04-30 23:36:41 +0530

    sdk name change

--- Package.swift
+++ Package.swift
@@ -12,10 +12,10 @@ let checksum = "c9966c3beb05add5115c2b1d4f6ee6de8ee48c67ca8c1f622003ee86130582cf
 let package = Package(
     name: sdkName,
     products: [
-        .library(name: sdkName, targets: [name]),
+        .library(name: name, targets: [sdkName]),
     ],
     targets: [
-            .binaryTarget(name: name, url: url, checksum: checksum),
+            .binaryTarget(name: sdkName, url: url, checksum: checksum),
 //            .binaryTarget(name: name, path: "../output/\(name).xcframework")
         ],
         swiftLanguageVersions: [.v5]
